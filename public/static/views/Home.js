92602bdc1a7fd7dff7fdd01e2cf822b61c3284fb
Mug3n96 <heinrich.root@outlook.de>
2021-09-06 02:09:17 +0200

    init base

--- public/static/views/Home.js
+++ public/static/views/Home.js
@@ -8,7 +8,18 @@ export default class extends AbstractView {
 
   async getHtml() {
     return `
-      <article class="article home">
+      <article class="game">
+        <header id="game_head">WPM: 90</header>
+        <main id="game_main">
+          <p id="placeholder">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type</p>
+        </main>
+        <footer id="game_footer">
+          <div id="game_diagramm" class="game-diagramm"></div>
+          <div class="game-footer-text__group">
+            <p>Words Typed: <span id="game_words_typed">100</span></p>
+            <p>Failures: <span id="game_failures">10</span></p>
+          </div>
+        </footer>
       </article>
     `;
   }
